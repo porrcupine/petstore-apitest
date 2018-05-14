62fe6d05971753f8123bb3178c19a362b06692c0
SK <sgkuzhel@gmail.com>
2018-05-14 10:25:56 +0300

    new test project started

---
+++ src/main/java/popo/Popopopo.java
@@ -0,0 +1,7 @@
+package popo;
+
+/**
+ * Created by SK on 2018-05-13
+ */
+public class Popopopo {
+}
---
+++ src/test/java/io/swagger/petstore/PetTest.java
@@ -0,0 +1,35 @@
+package io.swagger.petstore;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Created by SK on 2018-05-13
+ */
+public class PetTest {
+    String idTestValue = RandomStringUtils.randomNumeric(5);
+    String testPetsName = "Pett";
+
+    @Test
+    public void petApiTest() {
+        RestAssured.given()
+                .baseUri("http://petstore.swagger.io")
+                .basePath("/v2/pet")
+                .contentType(ContentType.JSON)
+                .header("api_key", "2233322")
+                .body("{\n" +
+                        " \"id\": " + idTestValue + ", \n " +
+                        " \"name\": \"" + testPetsName + "\", \n" +
+                        " \"photoUrls\": [],\n" +
+                        " \"tags\": [],\n " +
+                        " \"status\": \"pending\"\n" +
+                        "}")
+                .when().post()
+                .then()
+                .body("id", Matchers.equalTo(Integer.parseInt(idTestValue)));
+                //.then().extract().response().prettyPrint();
+    }
+}
